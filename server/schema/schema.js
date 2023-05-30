8ef296d658ad5af100337797330c777ac265dcec
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2023-05-30 13:46:18 +0530

    GraphQL Resolve Function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,13 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const _ = require("lodash");
+//Dummy Data
+
+var books = [
+  { name: "Book 1", id: "1" },
+  { name: "Book 2", id: "2" },
+  { name: "Book 3", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -10,20 +18,20 @@ const BookType = new GraphQLObjectType({
   }),
 });
 
-
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        book: {
-            type: BookType,
-            args: {id:{type: GraphQLString}},
-            resolve(parent, args) {
-                //Code to get Data from DB / Other Source
-            }
-        }
-    }
-})
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //Code to get Data from DB / Other Source
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
-})
+  query: RootQuery,
+});
