d15d48ceefff63f58bbfff885c357dc1f25ec457
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2023-05-30 12:35:38 +0530

    Setup GraphQl

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
 
+app.use("/graphql", graphqlHTTP({}));
+
 app.listen(8000, () => {
-  console.log("Server is    Up and Runn ing at Port 8000");
+  console.log("Server is Up and Running at Port 8000");
 });
